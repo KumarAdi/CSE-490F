7580f8f556ff6fb1b482889dbb69dc0f16125f89
Aditya Kumar <adikumar@cs.washington.edu>
2021-04-19 18:45:06 -0700

    Finished L3
    Also modified L2 based on L3 directions

--- L2_Blinking_an_LED/L2_Blinking_an_LED.ino
+++ L2_Blinking_an_LED/L2_Blinking_an_LED.ino
@@ -1,12 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(3, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(3, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("Pin 13 is HIGH");
   delay(1000);
-  digitalWrite(3, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("Pin 13 is LOW");
   delay(1000);
 }
