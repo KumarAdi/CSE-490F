ff043197636291da05b246231684b77a1e535be1
Aditya Kumar <adikumar@cs.washington.edu>
2021-04-26 23:54:47 -0700

    Week 4 L4
    Also some file modifications that I forgot to add to the previous move commit

--- Week_4_Output/L4_Potentiometers/L4_Potentiometers.ino
+++ Week_4_Output/L4_Potentiometers/L4_Potentiometers.ino
@@ -1,10 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  pinMode(3, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello world!");
-  delay(500);
+  const int brightness = map(analogRead(A0), 1023, 0, 0, 255);
+  analogWrite(3,  brightness);
 }
