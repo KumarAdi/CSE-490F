230422188b91cffff4b22c60f2ad4b55c8af9ca2
Aditya Kumar <adikumar@cs.washington.edu>
2021-04-28 13:03:28 -0700

    Photoresistor integrated As well

--- MP__Night_Light/MP__Night_Light.ino
+++ MP__Night_Light/MP__Night_Light.ino
@@ -1,22 +1,33 @@
 #include <CapacitiveSensor.h>
 
-const int csThreshold = 2000;
+const int CS_TRANSMIT_PIN = 13;
+const int CS_RECEIVE_PIN = 13;
+const int CS_THRESHOLD = 2000;
 
-CapacitiveSensor cs = CapacitiveSensor(13,12);
+const int PHOTORES_PIN = A0;
+
+CapacitiveSensor cs = CapacitiveSensor(CS_TRANSMIT_PIN,CS_RECEIVE_PIN);
 
 void setup() {
   // put your setup code here, to run once:
   cs.set_CS_AutocaL_Millis(0xFFFFFFFF);
   Serial.begin(9600); 
 
+  pinMode(PHOTORES_PIN, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  long sensedVal =  cs.capacitiveSensor(60);
-//  Serial.print("sensedVal:");
-//  Serial.println(sensedVal);
-  if (sensedVal > csThreshold) {
+  long touchVal =  cs.capacitiveSensor(60);
+//  Serial.print("touchVal:");
+//  Serial.println(touchVal);
+  
+  int photoVal = analogRead(PHOTORES_PIN);
+  Serial.print("photoVal:");
+  Serial.println(photoVal);
+
+  
+  if (touchVal > CS_THRESHOLD) {
     Serial.println("pressed");
   }
 }
