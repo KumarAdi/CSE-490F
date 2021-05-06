a349a1c1e9328c74b281b2915fcb4a109947e648
Aditya Kumar <adikumar@cs.washington.edu>
2021-05-06 04:29:51 -0700

    Started writing 3d render code
    Translation works, rotation has scaling issues, I suspect Rodrigues' rotation of being numerically unstable, but do not know for certain

---
+++ MP2_Microgame/MP2_Microgame.ino
@@ -0,0 +1,179 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define FOCAL_LENGTH 64
+
+#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 _display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+class Drawable {
+  public:
+  virtual void draw(Adafruit_SSD1306 _display) = 0;
+};
+
+class Point3: Drawable {
+  public:
+
+  Point3(int x, int y, int z) : x(x), y(y), z(z) {}
+
+  Point3() : x(0), y(0), z(0) {};
+
+  Point3 operator+ (const Point3& r) {
+    return Point3(this->x + r.x, this->y + r.y, this->z + r.z);
+  }
+
+  Point3 operator- (const Point3& r) {
+    return Point3(this->x - r.x, this->y - r.y, this->z - r.z);
+  }
+
+  Point3 operator* (float scale) {
+    return Point3(round(this->x * scale), round(this->y * scale), round(this->z * scale));
+  }
+
+  Point3 operator/ (float scale) {
+    return Point3(round(this->x / scale), round(this->y / scale), round(this->z / scale));
+  }
+
+  int dot(const Point3& r) {
+    return this->x * r.x + this->y * r.y + this->z * r.z;
+  }
+
+  Point3 cross (const Point3& r) {
+    return Point3(
+      this->y * r.z - this->z * r.y,
+      this->z * r.x - this->x * r.z,
+      this->x * r.y - this->y * r.x);
+  }
+
+  Point3 rotate (Point3 axis, float angle) {
+    float unchanged_factor = cos(angle);
+    float cross_factor = sin(angle);
+    float axis_factor = (1.0f - cos(angle)) * axis.dot(*this);
+
+    return *this * unchanged_factor + axis.cross(*this) * cross_factor + axis * axis_factor;
+  }
+  
+  void draw(Adafruit_SSD1306 _display) {
+    int x ,y;
+    this->toScreenCoords(&x, &y);
+
+    Serial.print("(");
+    Serial.print(x);
+    Serial.print(",");
+    Serial.print(y);
+    Serial.println(")");
+
+    _display.drawPixel(x, y, SSD1306_WHITE);
+  }
+
+  void toScreenCoords(int *x, int *y) {
+    *x = ((this->x * FOCAL_LENGTH) / (this->z + FOCAL_LENGTH)) + (SCREEN_WIDTH / 2);
+    *y = ((this->y * FOCAL_LENGTH) / (this->z + FOCAL_LENGTH)) + (SCREEN_HEIGHT / 2) ;
+  }
+  
+  private:
+  int x, y, z;
+};
+
+class Line3: Drawable {
+
+  public:
+  Line3 (Point3 a, Point3 b) {
+    this->a = a;
+    this->b = b;
+  }
+  
+  void draw(Adafruit_SSD1306 _display) {
+    int x_a, y_a;
+    a.toScreenCoords(&x_a, &y_a);
+    int x_b, y_b;
+    b.toScreenCoords(&x_b, &y_b);
+
+    _display.drawLine(x_a, y_a, x_b, y_b, SSD1306_WHITE);
+  }
+  
+  private:
+  Point3 a, b;
+};
+
+class Cube3: Drawable {
+  public:
+  Cube3(Point3 pos, Point3 down, Point3 right, Point3 forward): pos(pos), down(down), right(right), forward(forward) {}
+
+   void draw(Adafruit_SSD1306 _display) {
+    Point3 front_bottom_right = this->pos + this-> forward + this->down + this->right;
+    Point3 front_bottom_left  = this->pos + this-> forward + this->down - this->right;
+    Point3 front_top_right    = this->pos + this-> forward - this->down + this->right;
+    Point3 front_top_left     = this->pos + this-> forward - this->down - this->right;
+    Point3 back_bottom_right  = this->pos - this-> forward + this->down + this->right;
+    Point3 back_bottom_left   = this->pos - this-> forward + this->down - this->right;
+    Point3 back_top_right     = this->pos - this-> forward - this->down + this->right;
+    Point3 back_top_left      = this->pos - this-> forward - this->down - this->right;
+
+    Line3(front_bottom_right, front_bottom_left).draw(_display);
+    Line3(front_bottom_right, front_top_right).draw(_display);
+    Line3(front_bottom_right, back_bottom_right).draw(_display);
+
+    Line3(back_top_left, back_top_right).draw(_display);
+    Line3(back_top_left, back_bottom_left).draw(_display);
+    Line3(back_top_left, front_top_left).draw(_display);
+
+    Line3 (front_top_right, front_top_left).draw(_display);
+    Line3 (front_top_left, front_bottom_left).draw(_display);
+    Line3 (front_bottom_left, back_bottom_left).draw(_display);
+    Line3 (back_bottom_left, back_bottom_right).draw(_display);
+    Line3 (back_bottom_right, back_top_right).draw(_display);
+    Line3 (back_top_right, front_top_right).draw(_display);
+  }
+
+  void rotate(Point3 axis, float angle) {
+    this->down = this->down.rotate(axis, angle);
+    this->right = this->right.rotate(axis, angle);
+    this->forward = this->forward.rotate(axis, angle);
+  }
+
+  void move(Point3 displacement) {
+    this->pos = this->pos + displacement;
+  }
+
+  private:
+  Point3 pos, down, right, forward;
+};
+
+Point3 test_point_a(-64, -32, 16);
+Point3 test_point_b(-64, -32, 32);
+Line3 test_line(test_point_a, test_point_b);
+
+Cube3 test_cube(
+  Point3(0,0,64),
+  Point3(0,16,0),
+  Point3(16,0,0),
+  Point3(0,0,16)
+  );
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!_display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  _display.clearDisplay();
+  test_cube.draw(_display);
+  test_cube.rotate(Point3(0,0,1), 0.1f);
+//  test_cube.move(Point3(1,1,1));
+  _display.display();
+//  delay(10);
+}
