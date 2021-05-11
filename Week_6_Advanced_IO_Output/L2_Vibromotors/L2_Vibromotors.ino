46d65e543decb732e91815c246c494feb589a3ac
Aditya Kumar <adikumar@cs.washington.edu>
2021-05-11 00:15:52 -0700

    Week 6 L2 Part 2

--- Week_6_Advanced_IO_Output/L2_Vibromotors/L2_Vibromotors.ino
+++ Week_6_Advanced_IO_Output/L2_Vibromotors/L2_Vibromotors.ino
@@ -1,14 +1,16 @@
 #define VIBRO_PIN 6
+#define POT_PIN A1
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(VIBRO_PIN, OUTPUT);
+  pinMode(POT_PIN, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(VIBRO_PIN, HIGH);
-  delay(1000);
-  digitalWrite(VIBRO_PIN, LOW);
-  delay(1000);
+  const int pot_in = analogRead(POT_PIN);
+  const int vibro_out = map(pot_in, 0, 1024, 0, 255);
+  analogWrite(VIBRO_PIN, vibro_out);
+  delay(250);
 }
