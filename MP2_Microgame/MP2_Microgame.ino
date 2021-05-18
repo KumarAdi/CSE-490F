391d10f8a29863f0e314aa5bc9fda653aac2a760
Aditya Kumar <adikumar@cs.washington.edu>
2021-05-17 21:26:32 -0700

    Basic gameplay finished
    just need to add timer for game over condition

--- MP2_Microgame/MP2_Microgame.ino
+++ MP2_Microgame/MP2_Microgame.ino
@@ -2,9 +2,22 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Tone32.hpp>
+#include "src/JC_Button/JC_Button.h"
 
 #define UP_DOWN_PIN A1
 #define LEFT_RIGHT_PIN A0
+#define BUZZER_PIN 14
+#define LEFT_BTN_PIN 32
+#define RIGHT_BTN_PIN 15
+
+Button left_btn(LEFT_BTN_PIN, 50);
+Button right_btn(RIGHT_BTN_PIN, 50);
+
+#define TONE_OUTPUT_PIN 19
+#define TONE_PWM_CHANNEL 0
+
+Tone32 _tone32(TONE_OUTPUT_PIN, TONE_PWM_CHANNEL);
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -26,9 +39,14 @@ typedef enum _GameState {
   GAME_OVER = 2
 } GameState;
 
+typedef enum _Orientation {
+  LEFT,
+  RIGHT
+} Orientation;
+
 class Drawable {
   public:
-  virtual void draw(Adafruit_SSD1306& _display);
+  virtual void draw(Adafruit_SSD1306& _display) = 0;
 };
 
 class Point3: Drawable {
@@ -83,8 +101,17 @@ class Point3: Drawable {
     return ret;
   }
 
-  Point3 rotateAbout (Point3 axis, float angle, Point3 origin) {
+  void rotateAbout (Point3 axis, float angle, Point3 origin) {
     *this = origin + (*this - origin).rotate(axis, angle);
+  }
+
+  void screenInvariantSetZ (float z) {
+    int screenX, screenY;
+
+    this->toScreenCoords(&screenX, &screenY);
+    this->x = (screenX - (SCREEN_WIDTH / 2)) * (z + FOCAL_LENGTH) / FOCAL_LENGTH;
+    this->y = (screenY - (SCREEN_HEIGHT / 2)) * (z + FOCAL_LENGTH) / FOCAL_LENGTH;
+    this->z = z;
   }
   
   void draw(Adafruit_SSD1306& _display) {
@@ -103,19 +130,25 @@ class Point3: Drawable {
     return this->dot(*this);
   }
 
-  void println() {
+  void print() {
     Serial.print("(");
     Serial.print(x);
     Serial.print(", ");
     Serial.print(y);
     Serial.print(", ");
     Serial.print(z);
-    Serial.println(")");
+    Serial.print(")");
+  }
+
+  void println() {
+    this->print();
+    Serial.println();
   }
   
-  private:
   float x, y, z;
 };
+
+Point3 rotationCenter(0, 0, 64);
 
 class Line3: Drawable {
 
@@ -132,6 +165,26 @@ class Line3: Drawable {
     b.toScreenCoords(&x_b, &y_b);
 
     _display.drawLine(x_a, y_a, x_b, y_b, SSD1306_WHITE);
+  }
+
+  void rotateAbout(Point3 axis, float angle, Point3 origin) {
+    this->a.rotateAbout(axis, angle, origin);
+    this->b.rotateAbout(axis, angle, origin);
+  }
+
+  void screenInvariantSetZ(float z) {
+    this->a.screenInvariantSetZ(z);
+    this->b.screenInvariantSetZ(z);
+  }
+
+  void randomizeDepth(float minDepth, float maxDepth) {
+    float range = maxDepth - minDepth;
+    this->a.screenInvariantSetZ(minDepth + (random(100 * range) / 100));
+    this->b.screenInvariantSetZ(minDepth + (random(100 * range) / 100));
+  }
+
+  void println() {
+    Serial.print("Line:"); a.print(); b.println();  
   }
   
   private:
@@ -175,44 +228,98 @@ class Cube3: Drawable {
   }
 
   void rotateAbout(Point3 axis, float angle, Point3 origin) {
-    this->pos = this->pos.rotateAbout(axis, angle, origin);
-//    this->rotate(axis, angle);
+    this->pos.rotateAbout(axis, angle, origin);
   }
 
   void move(Point3 displacement) {
     this->pos = this->pos + displacement;
   }
 
-//  private:
+  private:
   Point3 pos, down, right, forward;
 };
 
+class Arrow3: Drawable {
+  public:
+  Arrow3 (Point3 center, int width, Orientation dir): shaft(Line3(center, center)), headA(Line3(center, center)), headB(Line3(center, center)) {
+    Point3 left = center - Point3(width /2, 0, 0);
+    Point3 right = center + Point3(width /2, 0, 0);
 
+    Point3 tip = (dir == LEFT) ? left : right;
+    Point3 base = (dir == LEFT) ? right : left;
 
-Cube3 test_cube_1(
-  Point3(0,0,96),
-  Point3(0,16,0),
-  Point3(16,0,0),
-  Point3(0,0,16)
-);
+    this->shaft = Line3(base, tip);
+    this->headA = Line3(tip, center + Point3(0, width / 2, 0));
+    this->headB = Line3(tip, center - Point3(0, width / 2, 0));
+    
+    this->shaft.screenInvariantSetZ(rotationCenter.z);
+    this->headA.screenInvariantSetZ(rotationCenter.z);
+    this->headB.screenInvariantSetZ(rotationCenter.z);
+    this->println();
+  }
+  
+  void draw(Adafruit_SSD1306& _display) {
+    this->shaft.draw(_display);
+    this->headA.draw(_display);
+    this->headB.draw(_display);
+  }
 
-Cube3 test_cube_2(
-  Point3(0,32,64),
-  Point3(0,16,0),
-  Point3(16,0,0),
-  Point3(0,0,16)
-);
+  void rotateAbout(Point3 axis, float angle, Point3 origin) {
+    this->shaft.rotateAbout(axis, angle, origin);
+    this->headA.rotateAbout(axis, angle, origin);
+    this->headB.rotateAbout(axis, angle, origin);
+  }
 
-Cube3 test_cube_3(
-  Point3(32,0,64),
-  Point3(0,16,0),
-  Point3(16,0,0),
-  Point3(0,0,16)
-);
+  void randomizeDepth(float minDepth, float maxDepth) {
+    this->shaft.randomizeDepth(minDepth, maxDepth);
+    this->headA.randomizeDepth(minDepth, maxDepth);
+    this->headB.randomizeDepth(minDepth, maxDepth);
+  }
+
+  void println() {
+    Serial.println("Arrow:");
+    Serial.print("shaft: "); shaft.println(); 
+    Serial.print("headA: "); headA.println(); 
+    Serial.print("headB: "); headB.println(); 
+  }
+  
+  private:
+  Line3 shaft, headA, headB;
+};
+
+//Cube3 test_cube_1(
+//  Point3(0,0,96),
+//  Point3(0,16,0),
+//  Point3(16,0,0),
+//  Point3(0,0,16)
+//);
+//
+//Cube3 test_cube_2(
+//  Point3(0,32,64),
+//  Point3(0,16,0),
+//  Point3(16,0,0),
+//  Point3(0,0,16)
+//);
+//
+//Cube3 test_cube_3(
+//  Point3(32,0,64),
+//  Point3(0,16,0),
+//  Point3(16,0,0),
+//  Point3(0,0,16)
+//);
+
+Orientation test_code[] = {LEFT, RIGHT, RIGHT, LEFT, LEFT}; 
+Arrow3 test_arrows[] = {
+  Arrow3(Point3(-32, 0, 0), 12, LEFT),
+  Arrow3(Point3(-16, 0, 0), 12, RIGHT),
+  Arrow3(Point3(0, 0, 0), 12, RIGHT),
+  Arrow3(Point3(16, 0, 0), 12, LEFT),
+  Arrow3(Point3(32, 0, 0), 12, LEFT)
+};
 
 int startMillis;
-GameState gameState;
-Cube3 gameObjects[3] = {test_cube_1, test_cube_2, test_cube_3};
+GameState gameState, prevState;
+//Cube3 gameObjects[3] = {test_cube_1, test_cube_2, test_cube_3};
 
 void setup() {
   // put your setup code here, to run once:
@@ -220,6 +327,10 @@ void setup() {
 
   pinMode(UP_DOWN_PIN, INPUT);
   pinMode(LEFT_RIGHT_PIN, INPUT);
+  pinMode(LEFT_BTN_PIN, INPUT_PULLUP);
+  pinMode(RIGHT_BTN_PIN, INPUT_PULLUP);
+
+  pinMode(BUZZER_PIN, OUTPUT);
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!_display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -227,26 +338,93 @@ void setup() {
     for(;;); // Don't proceed, loop forever
   }
 
-  gameState = PLAY;
+  gameState = MENU;
   startMillis = millis();
 
   _display.setTextSize(1);
   _display.setTextColor(WHITE, BLACK);
+
+  left_btn.begin();
+  right_btn.begin();
+
+
 }
 
-void runPlayState(int in_x, int in_y) {
-  for(Cube3 *curr = gameObjects; curr - gameObjects < 3; curr++) {
-   curr->rotateAbout(Point3(sgn(in_y),0, 0), in_y / 10240.0f, Point3(0, 0, 64));
-   curr->rotate(Point3(sgn(in_y),0, 0), in_y / 10240.0f);
-   curr->rotateAbout(Point3(0,sgn(in_x), 0), in_x / 10240.0f, Point3(0, 0, 64));
-   curr->rotate(Point3(0,sgn(in_x), 0), in_x / 10240.0f);
-   curr->draw(_display);
+long timeLeft = 60000;
+int curr_guess;
+
+void setupPlayState() {
+  for (int i = 0; i < 5; i++) {
+    test_code[i] = (random(255) > 127) ? LEFT : RIGHT;
+    test_arrows[i] = Arrow3(Point3(16 * i - 32, 0, 0), 12, test_code[i]);
+  }
+  
+  for (Arrow3 *curr = test_arrows; curr - test_arrows < 5; curr++) {
+   curr->randomizeDepth(rotationCenter.z / 2, rotationCenter.z * 3 / 2);
+  }
+
+  int randRotY = random(-300, 300);
+  int randRotX = random(-300, 300);
+
+  for (Arrow3 *curr = test_arrows; curr - test_arrows < 5; curr++) {
+    curr->rotateAbout(Point3(sgn(randRotY),0, 0), randRotY / 100.0f, rotationCenter);
+    curr->rotateAbout(Point3(0,sgn(randRotX), 0), randRotX / 100.0f, rotationCenter);
+  }
+
+  curr_guess = 0;
+}
+
+void runPlayState(int in_x, int in_y, bool left_btn, bool right_btn) {
+//  for(Cube3 *curr = gameObjects; curr - gameObjects < 3; curr++) {
+//   curr->rotateAbout(Point3(sgn(in_y),0, 0), in_y / 20480.0f, rotationCenter);
+//   curr->rotate(Point3(sgn(in_y),0, 0), in_y / 20480.0f);
+//   curr->rotateAbout(Point3(0,sgn(in_x), 0), in_x / 20480.0f, rotationCenter);
+//   curr->rotate(Point3(0,sgn(in_x), 0), in_x / 20480.0f);
+//   curr->draw(_display);
+//  }
+  Orientation guessedOrientation = (left_btn) ? LEFT : RIGHT;
+
+  if (left_btn || right_btn) {
+    if (test_code[curr_guess] != guessedOrientation) {
+      digitalWrite(BUZZER_PIN, HIGH);
+      curr_guess = 0;
+    } else {
+      _tone32.playNote(NOTE_B, 4, 250);
+      curr_guess++;
+
+      if (curr_guess >= 5) {
+        _tone32.playNote(NOTE_E, 5, 500);
+        setupPlayState();
+      }
+    }
+  } else {
+    digitalWrite(BUZZER_PIN, LOW);
+  }
+
+  for (Arrow3 *curr = test_arrows; curr - test_arrows < 5; curr++) {
+    curr->rotateAbout(Point3(sgn(in_y),0, 0), in_y / 20480.0f, rotationCenter);
+    curr->rotateAbout(Point3(0,sgn(in_x), 0), in_x / 20480.0f, rotationCenter);
+    curr->draw(_display);
   }
 }
 
 void loop() {
-  const int in_y = analogRead(UP_DOWN_PIN) - 512;
-  const int in_x = 512 - analogRead(LEFT_RIGHT_PIN);
+  int in_y = analogRead(UP_DOWN_PIN) - 2048;
+  int in_x = 2048 - analogRead(LEFT_RIGHT_PIN);
+
+  if (-150 < in_y && in_y < 150) {
+    in_y = 0;
+  }
+
+  if (-150 < in_x && in_x < 150) {
+    in_x = 0;
+  }
+
+  Point3(in_x, in_y, 0).println();
+
+  left_btn.read();
+  right_btn.read();
+  
   // put your main code here, to run repeatedly:
   _display.clearDisplay();
   
@@ -257,22 +435,30 @@ void loop() {
       _display.getTextBounds(MENU_TEXT, 0, 0, &x, &y, &textWidth, &textHeight);
       _display.setCursor(_display.width() / 2 - textWidth / 2, _display.height() / 2 - textHeight / 2);
       _display.print(MENU_TEXT);
+
+      if (left_btn.wasPressed() || right_btn.wasPressed()) {
+        gameState = PLAY;
+      }
       break;
     case PLAY:
-      runPlayState(in_x, in_y);
+      if (prevState != PLAY) {
+        setupPlayState();
+      }
+      runPlayState(in_x, in_y, left_btn.wasPressed(), right_btn.wasPressed());
       break;
     case GAME_OVER:
       _display.getTextBounds(GAME_OVER_TEXT, 0, 0, &x, &y, &textWidth, &textHeight);
       _display.setCursor(_display.width() / 2 - textWidth / 2, _display.height() / 2 - textHeight / 2);
       _display.print(GAME_OVER_TEXT);
+      
+      if (left_btn.wasPressed() || right_btn.wasPressed()) {
+        gameState = PLAY;
+      }
       break;
   }
 
-// State transistion Code
-//  if (millis() - startMillis >= 1000) {
-//    gameState = (gameState + 1) % 3;
-//    startMillis = millis();
-//  }
+  prevState = gameState;
 
   _display.display();
+  _tone32.update();
 }
